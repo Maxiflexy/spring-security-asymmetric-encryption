76cd7f21a41b2c9f071fa95d6d95b590e9e3894e
Onyekachi Ejemba <onyekachimaximillian@gmail.com>
2025-07-27 10:21:32 +0100

    added user entity

---
+++ src/main/java/com/maxiflexy/app/role/Role.java
@@ -0,0 +1,31 @@
+package com.maxiflexy.app.role;
+
+import com.maxiflexy.app.common.BaseEntity;
+import com.maxiflexy.app.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "ROLES")
+public class Role extends BaseEntity {
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
