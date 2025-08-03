260d9b361406ecbd49edb5989ed0139d6f8fc886
Onyekachi Ejemba <onyekachimaximillian@gmail.com>
2025-08-03 11:36:52 +0100

    added method security

---
+++ src/main/java/com/maxiflexy/app/todo/security/TodoSecurityService.java
@@ -0,0 +1,31 @@
+package com.maxiflexy.app.todo.security;
+
+import com.maxiflexy.app.todo.Todo;
+import com.maxiflexy.app.todo.TodoRepository;
+import com.maxiflexy.app.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TodoSecurityService {
+
+    private final TodoRepository todoRepository;
+
+    public boolean isTodoOwner(final String todoId) {
+        final Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        final String userId = ((User) authentication.getPrincipal()).getId();
+
+        final Todo todo = this.todoRepository.findById(todoId)
+                .orElseThrow(() -> new EntityNotFoundException("Todo is not found with id: " + todoId));
+
+        return todo.getUser().getId()
+                .equals(userId);
+    }
+}
